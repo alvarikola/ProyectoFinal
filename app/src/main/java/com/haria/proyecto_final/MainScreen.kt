24dfe465cb65ce196eb17f8c4fd3a49fc18b80e1
alvarikola <148550896+alvarikola@users.noreply.github.com>
2025-03-24 21:16:12 +0000

    creada pantalla principal

---
+++ app/src/main/java/com/haria/proyecto_final/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.haria.proyecto_final
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(context: ComponentActivity, navController: NavController) {
+    Text(
+        text = "Hello alvaro",
+    )
+}
