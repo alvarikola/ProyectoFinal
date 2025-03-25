b821a0499e38b34911efb777413753d8d2ae3060
E00010 <sistemas@canalgestionlanzarote.es>
2025-03-25 08:02:18 +0000

    realizada TopAppBar

--- app/src/main/java/com/haria/proyecto_final/TopAppBar.kt
+++ app/src/main/java/com/haria/proyecto_final/TopAppBar.kt
@@ -1,2 +1,33 @@
 package com.haria.proyecto_final
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(onNavigationClick: () -> Unit, title: String) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        ),
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+            }
+        },
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+            rememberTopAppBarState()
+        )
+    )
+}
