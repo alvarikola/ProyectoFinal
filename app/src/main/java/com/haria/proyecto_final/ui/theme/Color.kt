cb6b108694f7fef5ceaa4878f42b1c2b26dd6342
E00010 <sistemas@canalgestionlanzarote.es>
2025-03-25 08:02:01 +0000

    añadidos primera version de colores para la app

--- app/src/main/java/com/haria/proyecto_final/ui/theme/Color.kt
+++ app/src/main/java/com/haria/proyecto_final/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val negroFondo = Color(0xFF09090B)
+val colorTarjetas = Color(0xFF1C1917)
+val rosa = Color(0xFFE11D48)
+val colorLetra = Color(0xFFF2F2F2)
+val colorSubtitulo = Color(0xFF9A9AA3)
