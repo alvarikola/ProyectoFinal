140f40a7de0f6a0bd38b5f808f4ea6e363c99839
E00010 <sistemas@canalgestionlanzarote.es>
2025-03-25 07:53:45 +0000

    realizado main

--- app/src/main/java/com/haria/proyecto_final/MainScreen.kt
+++ app/src/main/java/com/haria/proyecto_final/MainScreen.kt
@@ -2,14 +2,40 @@ package com.haria.proyecto_final
 
 import android.content.Context
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
 
 @Composable
-fun MainScreen(context: ComponentActivity, navController: NavController) {
-    Text(
-        text = "Hello alvaro",
-    )
+fun MainScreen(context: ComponentActivity, navController: NavHostController) {
+    val drawerState = androidx.compose.material3.rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+
+
+    // Función para abrir el drawer
+    val openDrawer: () -> Unit = {
+        coroutineScope.launch { drawerState.open() }
+    }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Menu(context = context, navController = navController)
+        }
+    ) {
+        Scaffold(
+            topBar = { TopAppBar(onNavigationClick = openDrawer, title = "Canal 1") },
+            content = { innerPadding ->
+                // Contenido principal de la pantalla
+                ContentMain(innerPadding, context)
+            }
+        )
+    }
 }
