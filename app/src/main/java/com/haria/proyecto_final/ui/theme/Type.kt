8642601a9f8ee21fea02edf6da46d8b706eb59c4
E00010 <sistemas@canalgestionlanzarote.es>
2025-04-08 13:40:28 +0100

    cambiada tipografia

--- app/src/main/java/com/haria/proyecto_final/ui/theme/Type.kt
+++ app/src/main/java/com/haria/proyecto_final/ui/theme/Type.kt
@@ -2,19 +2,32 @@ package com.haria.proyecto_final.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.haria.proyecto_final.R
+
+
+val geistFontFamily = FontFamily(
+    Font(R.font.geist)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = geistFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = geistFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
