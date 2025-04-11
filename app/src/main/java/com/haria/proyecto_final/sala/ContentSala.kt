585549cfff41dd06d390a1a2778e1806016889ce
E00010 <sistemas@canalgestionlanzarote.es>
2025-04-11 08:53:34 +0100

    añadido conntenedor de musica

--- app/src/main/java/com/haria/proyecto_final/sala/ContentSala.kt
+++ app/src/main/java/com/haria/proyecto_final/sala/ContentSala.kt
@@ -1,24 +1,97 @@
 package com.haria.proyecto_final.sala
 
 import android.content.Context
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.flowlayout.FlowRow
+import com.haria.proyecto_final.R
+import com.haria.proyecto_final.SupabaseManager
+import com.haria.proyecto_final.data.Cancion
 
 @Composable
 fun ContentSala(innerPadding: PaddingValues, context: Context) {
-    Column(
+
+    var listaCanciones by remember { mutableStateOf<List<Cancion>>(emptyList()) }
+
+    LaunchedEffect(key1 = true) {
+        try {
+            val canciones = SupabaseManager.getCancionesPorEstilo("electronica")
+            Log.d("CancionesDebug", canciones.toString())
+            listaCanciones = canciones
+        } catch (e: Exception) {
+            Log.e("Error", "Error al obtener canciones: ${e.message}")
+        }
+    }
+
+    FlowRow(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text("Contenido de la sala")
+//        listaCanciones.forEach { cancion ->
+//            Text(
+//                text = "Nombre de la canción: ${cancion.nombre}, Estilo: ${cancion.estilo}",
+//                modifier = Modifier.padding(8.dp),
+//                fontSize = 20.sp
+//            )
+//            if (cancion.imagenUrl == null) {
+//                Log.d("CancionImagen", "URL de la imagen: ${cancion.imagenUrl}")
+//            } else {
+//                Image(
+//                    painter = rememberImagePainter(data = cancion.imagenUrl),
+//                    contentDescription = null,
+//                    contentScale = ContentScale.Crop
+//                )
+//            }
+//        }
+        ContenedorMusica("rock")
+        ContenedorMusica("electronica")
+        ContenedorMusica("hiphop")
+        ContenedorMusica("pop")
+    }
+}
+
+@Composable
+fun ContenedorMusica(estilo: String) {
+    var icon:Painter = painterResource(id = R.drawable.portada_generica)
+    when (estilo) {
+        "rock" -> {
+            icon = painterResource(id = R.drawable.portada_rock)
+        }
+        "electronica" -> {
+            icon = painterResource(id = R.drawable.portada_electronic)
+        }
+        "hiphop" -> {
+            icon = painterResource(id = R.drawable.portada_hiphop)
+        }
+        "pop" -> {
+            icon = painterResource(id = R.drawable.portada_pop)
+        }
+    }
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .padding(16.dp),
+    ) {
+        Image(
+            painter = icon,
+            contentDescription = "Icono de aplicación",
+            Modifier.fillMaxSize(),
+        )
     }
 }
