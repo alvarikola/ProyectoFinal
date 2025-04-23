cf2574fb51db9bce2c7a218c6dafd8b5a0e1f180
E00010 <sistemas@canalgestionlanzarote.es>
2025-04-23 10:59:56 +0100

    actualizado data class perfil

--- app/src/main/java/com/haria/proyecto_final/data/Perfl.kt
+++ app/src/main/java/com/haria/proyecto_final/data/Perfl.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Perfil(
-    val UID: String,
+    val id: String,
     val created_at: String? = null,
     val nombre: String? = null,
     val email: String? = null
