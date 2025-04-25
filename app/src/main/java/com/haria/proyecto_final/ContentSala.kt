d22fd4df67ae9bcead286ddff1a15d92e6dbd40c
E00010 <sistemas@canalgestionlanzarote.es>
2025-04-25 14:47:17 +0100

    pruebas pantalla sala

--- app/src/main/java/com/haria/proyecto_final/ContentSala.kt
+++ app/src/main/java/com/haria/proyecto_final/ContentSala.kt
@@ -1,2 +1,53 @@
 package com.haria.proyecto_final
 
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.haria.proyecto_final.data.Cancion
+import com.haria.proyecto_final.data.Perfil
+
+@Composable
+fun ContentSala(innerPadding: PaddingValues, context: Context) {
+    var perfil by remember { mutableStateOf<Perfil?>(null) }
+    var cancion by remember { mutableStateOf<Cancion?>(null) }
+    LaunchedEffect(key1 = true) {
+        try {
+            perfil = SupabaseManager.getPerfil()
+            cancion = perfil?.trackid?.let { SupabaseManager.getCancionPorId(it) }
+        } catch (e: Exception) {
+            Log.e("Error", "Error al obtener el perfil: ${e.message}")
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        if(cancion != null) {
+            AsyncImage(
+                model = cancion!!.imagenUrl,
+                contentDescription = cancion!!.nombre ?: "Portada de la canción",
+                modifier = Modifier.size(250.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
--- app/src/main/java/com/haria/proyecto_final/SalaScreen.kt
+++ app/src/main/java/com/haria/proyecto_final/SalaScreen.kt
@@ -1,2 +1,20 @@
 package com.haria.proyecto_final
 
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.haria.proyecto_final.data.Cancion
+import com.haria.proyecto_final.menu.TopAppBar
+import com.haria.proyecto_final.seleccionMusica.ContentMusica
+
+@Composable
+fun SalaScreen(context: ComponentActivity, navController: NavHostController) {
+    Scaffold(
+        topBar = { TopAppBar(navController) },
+        content = { innerPadding ->
+            // Contenido principal de la pantalla
+            ContentSala(innerPadding, context)
+        }
+    )
+}
