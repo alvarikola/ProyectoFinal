7f09798a485e227f2304d9154d860d3032370e4a
E00010 <sistemas@canalgestionlanzarote.es>
2025-04-29 14:57:08 +0100

    mejorada interfaz de login

--- app/src/main/java/com/haria/proyecto_final/LoginScreen.kt
+++ app/src/main/java/com/haria/proyecto_final/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.haria.proyecto_final
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -26,9 +27,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.haria.proyecto_final.SupabaseManager.login
 import com.haria.proyecto_final.SupabaseManager.register
 import kotlinx.coroutines.launch
@@ -41,6 +45,7 @@ fun LoginScreen(onLoginSuccess: () -> Unit = {}) {
     var errorMessage by remember { mutableStateOf("") }
     var isLoading by remember { mutableStateOf(false) }
     var isRegisterMode by remember { mutableStateOf(false) }
+    val icon = painterResource(id = R.drawable.logo_circular)
 
     val scope = rememberCoroutineScope()
 
@@ -48,13 +53,19 @@ fun LoginScreen(onLoginSuccess: () -> Unit = {}) {
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
-            .padding(24.dp),
+            .padding(horizontal = 32.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = icon,
+            contentDescription = "Icono de aplicación",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(200.dp).padding(bottom = 24.dp),
+        )
         Text(
             if (isRegisterMode) "Registro" else "Log-in",
-            fontSize = 28.sp,
+            fontSize = 26.sp,
             color = MaterialTheme.colorScheme.onBackground
         )
         Spacer(modifier = Modifier.height(24.dp))
