b3b736fda48c209f597e104b8c04db55956feb9e
E00010 <sistemas@canalgestionlanzarote.es>
2025-05-06 15:05:54 +0100

    priemra interfaz chat

--- app/src/main/java/com/haria/proyecto_final/ContentSala.kt
+++ app/src/main/java/com/haria/proyecto_final/ContentSala.kt
@@ -3,17 +3,29 @@ package com.haria.proyecto_final
 import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -24,14 +36,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
+import com.google.accompanist.flowlayout.FlowRow
 import com.haria.proyecto_final.data.Cancion
+import com.haria.proyecto_final.data.Message
 import com.haria.proyecto_final.data.Perfil
 import com.haria.proyecto_final.estiloCancion.PlayerAction
 import com.haria.proyecto_final.musicaService.MusicViewModel
@@ -101,33 +117,54 @@ fun ContentSala(innerPadding: PaddingValues, context: Context, perfilId: String,
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            if (cancion != null) {
-                AsyncImage(
-                    model = cancion!!.imagenUrl,
-                    contentDescription = cancion!!.nombre ?: "Portada de la canción",
-                    modifier = Modifier.size(250.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            cancion?.nombre?.let {
-                Text(
-                    text = it,
-                    fontSize = 30.sp,
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
-            cancion?.cantante?.let {
-                Text(
-                    text = it,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
-            // TODO interfaz de chatgpt: chat para hablar y quitar musica al salir
             Row(
                 modifier = Modifier
-                    .padding(12.dp)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .weight(0.4f),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    if (cancion != null) {
+                        AsyncImage(
+                            model = cancion!!.imagenUrl,
+                            contentDescription = cancion!!.nombre ?: "Portada de la canción",
+                            modifier = Modifier
+                                .weight(0.7f),
+                            contentScale = ContentScale.Fit
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .weight(0.3f)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        cancion?.nombre?.let {
+                            Text(
+                                text = it,
+                                fontSize = 30.sp,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+                        cancion?.cantante?.let {
+                            Text(
+                                text = it,
+                                fontSize = 24.sp,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.05f),
                 horizontalArrangement = Arrangement.SpaceAround,
             ) {
                 Text(
@@ -141,33 +178,135 @@ fun ContentSala(innerPadding: PaddingValues, context: Context, perfilId: String,
                     modifier = Modifier.padding(8.dp)
                 )
             }
-            if (isPlaying) {
-                Button(
-                    onClick = { onAction(PlayerAction.Pause, perfil?.trackid ?: 0, null) },
-                    shape = CircleShape,
-                    contentPadding = PaddingValues(0.dp),
-                    modifier = Modifier.size(60.dp) // Tamaño del botón redondo
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_pause),
-                        contentDescription = "pausa",
-                        modifier = Modifier.size(35.dp)
-                    )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.05f),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                if (isPlaying) {
+                    Button(
+                        onClick = { onAction(PlayerAction.Pause, perfil?.trackid ?: 0, null) },
+                        shape = CircleShape,
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(60.dp) // Tamaño del botón redondo
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_pause),
+                            contentDescription = "pausa",
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+                } else {
+                    Button(
+                        onClick = { onAction(PlayerAction.Play, perfil?.trackid ?: 0, null) },
+                        shape = CircleShape,
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier.size(60.dp) // Tamaño del botón redondo
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_play),
+                            contentDescription = "Play",
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
                 }
-            } else {
-                Button(
-                    onClick = { onAction(PlayerAction.Play, perfil?.trackid ?: 0, null) },
-                    shape = CircleShape,
-                    contentPadding = PaddingValues(0.dp),
-                    modifier = Modifier.size(60.dp) // Tamaño del botón redondo
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_play),
-                        contentDescription = "Play",
-                        modifier = Modifier.size(35.dp)
-                    )
+            }
+            Row(
+                modifier = Modifier
+                    .weight(0.5f)
+            ) {
+                Column {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 15.dp, top = 15.dp, end = 15.dp, bottom = 5.dp)
+                    ) {
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(MaterialTheme.colorScheme.primary)
+                        )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                    ) {
+                        ChatScreen()
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ChatScreen() {
+    var messages by remember { mutableStateOf(listOf<Message>()) }
+    var input by remember { mutableStateOf(TextFieldValue("")) }
+    var user by remember { mutableStateOf<Perfil?>(null) }
+
+    LaunchedEffect(key1 = true) {
+        user = SupabaseManager.getPerfil()
+    }
+
+    Column(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            reverseLayout = true
+        ) {
+            items(messages.reversed()) { message ->
+                MessageBubble(message)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = input,
+                onValueChange = { input = it },
+                modifier = Modifier.weight(1f),
+                placeholder = { Text("Escribe un mensaje...") }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = {
+                    if (input.text.isNotBlank()) {
+                        messages = messages + Message(user?.nombre, input.text)
+                        input = TextFieldValue("")
+                    }
+                }
+            ) {
+                Text("Enviar")
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageBubble(message: Message) {
+    val userDefecto by remember { mutableStateOf("User${(1..100).random()}") }
+
+    FlowRow (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Text(
+            text = (message.user ?: userDefecto) + ": ",
+            color = Color.Red
+        )
+        Text(
+            text = message.text
+        )
     }
 }
---
+++ app/src/main/java/com/haria/proyecto_final/data/Mensaje.kt
@@ -0,0 +1,3 @@
+package com.haria.proyecto_final.data
+
+data class Message(val user: String?, val text: String)
