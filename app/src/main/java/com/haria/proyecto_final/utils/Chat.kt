7afd4dcab258ecbcb2f8bf926c450c195fe4b9b2
E00010 <sistemas@canalgestionlanzarote.es>
2025-05-09 13:09:29 +0100

    boton para emojis

--- app/src/main/java/com/haria/proyecto_final/utils/Chat.kt
+++ app/src/main/java/com/haria/proyecto_final/utils/Chat.kt
@@ -1,9 +1,12 @@
 package com.haria.proyecto_final.utils
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,10 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -29,8 +43,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.haria.proyecto_final.R
 import com.haria.proyecto_final.SupabaseManager
 import com.haria.proyecto_final.data.Mensaje
 import com.haria.proyecto_final.data.Perfil
@@ -43,6 +59,7 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.encodeToJsonElement
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Chat(userId: String) {
     val mensajes = remember { mutableStateListOf<Mensaje>() }
@@ -51,6 +68,10 @@ fun Chat(userId: String) {
     val coroutineScope = rememberCoroutineScope()
     val userColors = remember { mutableStateMapOf<String, Color>() }
     var subscribersCount by remember { mutableStateOf(0) }
+    var showEmojiMenu by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val emojis = listOf("😀", "😂", "😊", "😎", "❤️", "🔥", "👍", "🎉", "🥳", "😍", "😢", "😭", "😡", "😴", "🤔", "🤖", "👋", "🙌", "✌️", "🙏")
+
 
     // Crear canal
     val channel = SupabaseManager.obtenerCanal(userId)
@@ -98,13 +119,57 @@ fun Chat(userId: String) {
         }
     }
 
-
-    Column(modifier = Modifier.fillMaxSize().padding(8.dp)) {
-
-        Text(
-            text = "Usuarios en línea: $subscribersCount",
-            modifier = Modifier.padding(8.dp)
+    // Modal Bottom Sheet para emojis
+    if (showEmojiMenu) {
+        ModalBottomSheet(
+            onDismissRequest = { showEmojiMenu = false },
+            sheetState = rememberModalBottomSheetState(),
+            shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+            containerColor = Color.White,
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+                        items(emojis.chunked(4)) { row ->
+                            Row {
+                                row.forEach { emoji ->
+                                    Text(
+                                        text = emoji,
+                                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .clickable {
+                                                input = input.copy(text = input.text + emoji)
+                                                showEmojiMenu = false
+                                            }
+                                            .padding(8.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         )
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Usuarios en línea: $subscribersCount"
+            )
+        }
 
         LazyColumn(
             modifier = Modifier
@@ -127,10 +192,16 @@ fun Chat(userId: String) {
                 value = input,
                 onValueChange = {
                     input = it
-                    Log.i("prueba", "Nuevo valor: ${it.text}")
                 },
                 modifier = Modifier.weight(1f),
-                placeholder = { Text("Escribe un mensaje...") }
+                placeholder = { Text("Escribe un mensaje") },
+                trailingIcon = {
+                    Icon(
+                        Icons.Default.Face,
+                        contentDescription = "emoji",
+                        modifier = Modifier.clickable(onClick = { showEmojiMenu = true })
+                    )
+                }
             )
             Spacer(modifier = Modifier.width(8.dp))
             Button(
@@ -150,7 +221,11 @@ fun Chat(userId: String) {
                     }
                 }
             ) {
-                Text("Enviar")
+                Icon(
+                    imageVector = Icons.Default.Send,
+                    contentDescription = "Enviar",
+                    tint = Color.White
+                )
             }
         }
     }
