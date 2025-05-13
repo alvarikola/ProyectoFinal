f989e85e0a95ffb537082696b39be2c965dbfc8d
E00010 <sistemas@canalgestionlanzarote.es>
2025-05-13 10:16:46 +0100

    interfaz de los mensajes mejorada

--- app/src/main/java/com/haria/proyecto_final/utils/MensajeBubble.kt
+++ app/src/main/java/com/haria/proyecto_final/utils/MensajeBubble.kt
@@ -1,21 +1,31 @@
 package com.haria.proyecto_final.utils
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
 import com.haria.proyecto_final.data.Emote
 import com.haria.proyecto_final.data.Mensaje
+
 
 @Composable
 fun MessageBubble(mensaje: Mensaje, userColors: MutableMap<String, Color>, imageLoader: ImageLoader) {
@@ -24,20 +34,20 @@ fun MessageBubble(mensaje: Mensaje, userColors: MutableMap<String, Color>, image
     val userColor = userColors.getOrPut(userName) { generateRandomColor() }
     val text = splitTextWithEmojis(mensaje.text)
 
-    FlowRow (
+    FlowRow(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp),
+        crossAxisAlignment = FlowCrossAxisAlignment.End //Alineado abajo
     ) {
         Text(
             text = (mensaje.userNombre ?: userDefecto) + ": ",
             color = userColor,
             fontWeight = FontWeight.Bold
         )
-        text.forEach{
-            if(it.startsWith("#emoji:") && it.endsWith("#")) {
+        text.forEach {
+            if (it.startsWith("#emoji:") && it.endsWith("#")) {
                 val emoteId = it.substring(7, it.length - 1)
-                Log.i("Mensaje", "Emote ID: $emoteId")
                 AVIFEmoteWithLoader(emoteId, imageLoader = imageLoader)
             } else {
                 Text(
